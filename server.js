d0fdc6c827ef31894266b188432f7dd9af9a92b4
abdulcoder207 <abdulmuizridwan158@gmail.com>
2025-03-16 11:30:58 +0700

    add text server

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const fs = require('fs');
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 app.use(cors());
+
 app.get('/',(req,res) =>{
-    res.send("server berjalan normal")
-})
+    res.send("server berjalan normal langsung Ketik /api pada bagian url");
+});
 app.get('/api', (req, res) => {
     fs.readFile(__dirname + '/motivasi.json', 'utf8', (err, data) =>{
         if(err){
-            return res.status(500).json({error:"gagal baca file"})
+            return res.status(500).json({error:"gagal baca file"});
         };
         try{
             const quotes = JSON.parse(data);
@@ -20,10 +22,11 @@ app.get('/api', (req, res) => {
             res.json(quotes);
         }catch(parseError){
             console.log("Error parsing ", parseError);
-            res.status(500).json({error: "format json tidak valid"})
-        }
-    })
-})
+            res.status(500).json({error: "format JSON tidak valid"});
+        };
+    });
+});
+
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
-})
+    console.log(`http://localhost:${PORT}`);
+});
